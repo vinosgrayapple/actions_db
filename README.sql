7be949a9f1e3881deefff0629f2d17b10ee77776
Serhii Komarychev <komarichev@gmail.com>
2020-12-07 00:41:49 +0200

    :tada:

--- README.sql
+++
@@ -1,7 +0,0 @@
-INSERT INTO users(fname,lname,email,phone) VALUES ('Наталья', 'Богоявленскя','n.bogoyavlenskaya@artwinery.com.ua','+380664935171'); 
-
-UPDATE users
-SET phone = '+380664935171'
-WHERE lname='Богоявленскя';
-
-SELECT * from users;
